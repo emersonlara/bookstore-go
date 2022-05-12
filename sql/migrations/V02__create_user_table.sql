ce33afacdba281555c701d39f67ff6f950cb9fa1
Normandes Junior <normandes.junior@zup.com.br>
2022-05-12 14:07:45 -0300

    insert into database

--- sql/migrations/V02__create_user_table.sql
+++ sql/migrations/V02__create_user_table.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS users_db.users (
-   user_id SERIAL PRIMARY KEY,
+   id SERIAL PRIMARY KEY,
    first_name VARCHAR(100) NOT NULL,
-   last_name VARCHAR(100) NOT NULL
+   last_name VARCHAR(100),
+   email VARCHAR(100) NOT NULL UNIQUE,
+   date_created VARCHAR(100) NOT NULL
 );
--- user-api/domain/users/user_dao.go
+++ user-api/domain/users/user_dao.go
@@ -3,8 +3,11 @@ package users
 import (
 	"fmt"
 	"user-api/datasources/postgresql/users_db"
-	"user-api/utils/date_utils"
 	"user-api/utils/errors"
+)
+
+const (
+	insertUserQuery = "INSERT INTO users_db.users(first_name, last_name, email, date_created) VALUES ($1, $2, $3, $4) RETURNING id"
 )
 
 var (
@@ -31,17 +34,19 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	err := users_db.Client.QueryRow(insertUserQuery, user.FirstName, user.LastName, user.Email, user.DateCreated).Scan(&user.Id)
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("1. error when trying to save user: %s", err.Error()))
 	}
 
-	user.DateCreated = date_utils.GetNowString()
-
-	usersDB[user.Id] = user
+	// current := usersDB[user.Id]
+	// if current != nil {
+	// 	if current.Email == user.Email {
+	// 		return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
+	// 	}
+	// 	return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	// }
+	// user.DateCreated = date_utils.GetNowString()
+	// usersDB[user.Id] = user
 	return nil
 }
